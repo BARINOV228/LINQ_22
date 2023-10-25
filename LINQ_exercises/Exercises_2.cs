ca998e3463186ccb792c8fc6acf4665e41a87c4e
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-10-25 18:22:17 +0500

    Added

--- LINQ_exercises/Exercises_2.cs
+++ LINQ_exercises/Exercises_2.cs
@@ -86,39 +86,73 @@
 
             // 25 ---------------
 
-           List<Fruits> itemlist = new List<Fruits>
-           {
-           new Fruits { Id = 1, Name = "Biscuit  " },
-           new Fruits { Id = 2, Name = "Chocolate" },
-           new Fruits { Id = 3, Name = "Butter   " },
-           new Fruits { Id = 4, Name = "Brade    " },
-           new Fruits { Id = 5, Name = "Honey    " }
-           };
+            //    List<Fruits> itemlist = new List<Fruits>
+            //    {
+            //    new Fruits { Id = 1, Name = "Biscuit  " },
+            //    new Fruits { Id = 2, Name = "Chocolate" },
+            //    new Fruits { Id = 3, Name = "Butter   " },
+            //    new Fruits { Id = 4, Name = "Brade    " },
+            //    new Fruits { Id = 5, Name = "Honey    " }
+            //    };
 
-           List<Purchase> purchlist = new List<Purchase>
-           {
-           new Purchase { PurchaseId=100, Id = 3,  Cost = 800 },
-           new Purchase { PurchaseId=101, Id = 2,  Cost = 650 },
-           new Purchase { PurchaseId=102, Id = 3,  Cost = 900 },
-           new Purchase { PurchaseId=103, Id = 4,  Cost = 700 },
-           new Purchase { PurchaseId=104, Id = 3,  Cost = 900 },
-           new Purchase { PurchaseId=105, Id = 4,  Cost = 650 },
-           new Purchase { PurchaseId=106, Id = 1,  Cost = 458 }
-           };
+            //    List<Purchase> purchlist = new List<Purchase>
+            //    {
+            //    new Purchase { PurchaseId=100, Id = 3,  Cost = 800 },
+            //    new Purchase { PurchaseId=101, Id = 2,  Cost = 650 },
+            //    new Purchase { PurchaseId=102, Id = 3,  Cost = 900 },
+            //    new Purchase { PurchaseId=103, Id = 4,  Cost = 700 },
+            //    new Purchase { PurchaseId=104, Id = 3,  Cost = 900 },
+            //    new Purchase { PurchaseId=105, Id = 4,  Cost = 650 },
+            //    new Purchase { PurchaseId=106, Id = 1,  Cost = 458 }
+            //    };
 
-           var innerJoin = from e in itemlist
-                           join d in purchlist on e.Id equals d.Id
-                           select new
-                           {
-                               itid = e.Id,
-                               itdes = e.Name,
-                               prqty = d.Cost
-                           };
+            //    var innerJoin = from e in itemlist
+            //                    join d in purchlist on e.Id equals d.Id
+            //                    select new
+            //                    {
+            //                        itid = e.Id,
+            //                        itdes = e.Name,
+            //                        prqty = d.Cost
+            //                    };
 
-            foreach (var data in innerJoin)
-            {
-                Console.WriteLine(data.itid + "\t\t" + data.itdes + "\t\t" + data.prqty);
-            }
+            //    foreach (var data in innerJoin)
+            //    {
+            //        Console.WriteLine(data.itid + "\t\t" + data.itdes + "\t\t" + data.prqty);
+            //    }
+
+            // 28 ---------------
+
+            //         string[] cities = new string[] { "Toshkent", "Samarqand", "Buxoro", "Xorazm", "Farg'ona" };
+
+            //         IEnumerable<string> cityOrder = cities.OrderBy(str => str.Length)
+            //                                               .ThenBy(str => str);
+
+            //foreach (string city in cityOrder)
+            //	Console.WriteLine(city);
+
+            // 30 ---------------
+
+            //static List<Fruits> GetItemMast()
+            //{
+            //    List<Fruits> itemlist = new List<Fruits>();
+            //    itemlist.Add(new Fruits() { Id = 1, Name = "Biscuit  " });
+            //    itemlist.Add(new Fruits() { Id = 2, Name = "Honey    " });
+            //    itemlist.Add(new Fruits() { Id = 3, Name = "Butter   " });
+            //    itemlist.Add(new Fruits() { Id = 4, Name = "Brade    " });
+            //    itemlist.Add(new Fruits() { Id = 5, Name = "Honey    " });
+            //    itemlist.Add(new Fruits() { Id = 6, Name = "Biscuit  " });
+            //    return itemlist;
+            //}
+
+            //var itemlist = (from c in GetItemMast()
+            //                select c.Name)
+            //        .Distinct()
+            //        .OrderBy(x => x);
+
+            //foreach( var item in itemlist )
+            //{
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
